f01b7e773f6dfd3420a533657ad1f3c010f79a6f
chr1swill <alexanderwilliams20003@gmail.com>
2026-02-03 07:32:12 -0500

    feat: start with golang switching to hono.js

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "log"
+import (
+  "log"
+	"net/http"
+	"text/template"
+)
 
 func main () {
-	log.Println("hello");
+	var err error;
+	var mux *http.ServeMux;
+	var tmpl *template.Template;
+
+	tmpl, err = tmpl.ParseGlob("./html/");
+	if err != nil {
+		panic(err);
+	}
+
+	mux = http.NewServeMux();
+	log.Println(mux, tmpl);
 }
