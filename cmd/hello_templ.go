051ac242e38548b2f87c67b308c52068bf4c4636
chr1swill <alexanderwilliams20003@gmail.com>
2026-02-05 07:30:48 -0500

    feat: get it to compile

--- cmd/hello_templ.go
+++ cmd/hello_templ.go
@@ -8,7 +8,7 @@ package main
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Hello(name string) templ.Component {
+func LandingPage(name string) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -29,57 +29,20 @@ func Hello(name string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<button>Hire Me '")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html><head><style>\n\t\t\t  body{background-color: blue; color: grey;}\n\t\t\t\tbutton{background-color: grey; colour: blue;}\n\t\t\t</style><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>Nice to meet your ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(name)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/hello.templ`, Line: 4, Col: 24}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/hello.templ`, Line: 11, Col: 107}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "'</button>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		return nil
-	})
-}
-
-func LandingPage(name string) templ.Component {
-	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
-		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
-		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
-			return templ_7745c5c3_CtxErr
-		}
-		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
-		if !templ_7745c5c3_IsBuffer {
-			defer func() {
-				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
-				if templ_7745c5c3_Err == nil {
-					templ_7745c5c3_Err = templ_7745c5c3_BufErr
-				}
-			}()
-		}
-		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var3 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var3 == nil {
-			templ_7745c5c3_Var3 = templ.NopComponent
-		}
-		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<html><head></head><body>")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = Hello(name).Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</title></head><body></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,14 @@
 package main
 
 import (
-  "context"
+  "github.com/a-h/templ"
 	"net/http"
-	"fmt"
 	"log"
-	"os"
 )
 
 func main() {
-	dirName := "html";
-	_, err := os.Stat(dirName);
-	if err != nil {
-    err := os.Mkdir(dirName, os.FileMode(0755));
-		if err != nil {
-			log.Fatalf("failed to create directory '%s/': %v\n");
-		}
-	}
-		
-	f, err := os.OpenFile(fmt.Sprintf("%s/hello.html", dirName), os.O_RDWR|os.O_CREATE, 0644);
-	if err != nil {
-		log.Fatalf("failed to create file '%s/hello.html': %v\n", dirName, err);
-	}
-	defer f.Close();
-
   mux := http.NewServeMux();
-	mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-			err = LandingPage("John").Render(context.Background(), w);
-			if err != nil {
-			  log.Fatalf("failed to write output file: %v", err);
-			}
-	});
+	mux.Handle("/", templ.Handler(LandingPage("Baby")));
 
   log.Printf("serve starting on port :8080...");
 	if err := http.ListenAndServe(":8080", mux);err != nil {
